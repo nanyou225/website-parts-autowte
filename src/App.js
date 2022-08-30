19d8f6a10d2466c82ccb61e32ebd9140ed8b9824
noman <nomanplacidenoman@gmail.com>
2022-08-30 23:17:34 +0100

    F02 feat Add Styled components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Connexion from "./components/Connexion";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import Panier from "./components/Panier";
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import Connexion from "./pages/Connexion";
+// import Contact from "./pages/Contact";
+// import Home from "./pages/Home";
+// import Panier from "./pages/Panier";
 
 function App() {
   return (
-    <Routes>
+    {/*<Routes>
       <Route path="/" element={<Home />} exact />
       <Route path="/contact" element={<Contact />} exact />
       <Route path="/connexion" element={<Connexion />} exact />
       <Route path="/panier" element={<Panier />} exact />
-    </Routes>
+    </Routes>*/}
   );
 }
 
